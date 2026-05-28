1eea2c1d12ed6929fa7b8116c45211cf1e1776ee
Tpchck <tpchkcrew@gmail.com>
2026-05-28 10:59:09 +0200

    feat: add cron route to prevent database sleep and exclude it from middleware authentication

--- middleware.ts
+++ middleware.ts
@@ -78,6 +78,7 @@ export async function middleware(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/register") &&
     !request.nextUrl.pathname.startsWith("/api/auth") &&
+    !request.nextUrl.pathname.startsWith("/api/cron") &&
     request.nextUrl.pathname !== "/"
   ) {
     const url = request.nextUrl.clone();
